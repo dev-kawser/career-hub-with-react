f83b91b12b941d395584fa4e727a11fc6a55dd31
devsafi <mdkawserferdoussafi@gmail.com>
2024-03-22 21:49:34 +0600

    first commit

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+
+const ErrorPage = () => {
+    return (
+        <div className="max-w-7xl mx-auto text-center">
+            <h1 className="text-7xl font-bold text-red-600">404</h1>
+            <p className="text-3xl font-semibold">Data Not Found</p>
+            <Link to="/"><button className="btn btn-active btn-link text-lg">Go back to Home</button></Link>
+        </div>
+    );
+};
+
+export default ErrorPage;
